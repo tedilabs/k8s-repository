c3b78f56206a257074e3793901b3cc5a63dd1264
Byungjin Park <posquit0.bj@gmail.com>
2021-05-02 23:24:54 +0900

    Add cert-manager v1.0 and v1.2

---
+++ addons/cert-manager/download.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+
+DOWNLOAD_URL="https://github.com/jetstack/cert-manager/releases/download"
+DOWNLOAD_VERSION="v1.2.0"
+DOWNLOAD_DIRECTORY="$DOWNLOAD_VERSION"
+
+mkdir -p $DOWNLOAD_DIRECTORY
+curl -Ls "$DOWNLOAD_URL/$DOWNLOAD_VERSION/cert-manager.yaml" -o $DOWNLOAD_DIRECTORY/cert-manager.yaml
