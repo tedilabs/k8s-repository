78631fd24a1f22fbbf700b3831452826077aeeac
Byungjin Park <posquit0.bj@gmail.com>
2021-05-02 23:24:25 +0900

    Add aws-load-balancer-controller v2.1

---
+++ addons/aws-load-balancer-controller/download.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+
+DOWNLOAD_URL="https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/main/docs/install"
+DOWNLOAD_VERSION="v2.1.3"
+DOWNLOAD_DIRECTORY="$DOWNLOAD_VERSION"
+
+mkdir -p $DOWNLOAD_DIRECTORY
+curl -Ls "$DOWNLOAD_URL/${DOWNLOAD_VERSION//./_}_full.yaml" -o $DOWNLOAD_DIRECTORY/install.yaml
