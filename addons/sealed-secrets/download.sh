1fc8dfc88678acd5c6341c491389556c0261bfeb
Byungjin Park <posquit0.bj@gmail.com>
2021-05-02 23:25:51 +0900

    Add sealed-secrets v0.15

---
+++ addons/sealed-secrets/download.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+
+DOWNLOAD_VERSION="v0.15.0"
+DOWNLOAD_URL="https://github.com/bitnami-labs/sealed-secrets/releases/download/${DOWNLOAD_VERSION}/controller.yaml"
+DOWNLOAD_DIRECTORY="$DOWNLOAD_VERSION"
+
+mkdir -p $DOWNLOAD_DIRECTORY
+curl -Ls "$DOWNLOAD_URL" -o $DOWNLOAD_DIRECTORY/controller.yaml
