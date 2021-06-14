ebb2210ae9a321d2221c992cf44ef3ade6eb9efd
Byungjin Park <posquit0.bj@gmail.com>
2021-06-15 01:14:19 +0900

    Add argo-workflow v3.0

---
+++ addons/argo-workflow/download.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+
+DOWNLOAD_URL="https://api.github.com/repos/argoproj/argo-workflows/tarball"
+DOWNLOAD_VERSION="v3.0.7"
+DOWNLOAD_DIRECTORY="$DOWNLOAD_VERSION"
+
+curl -Ls "$DOWNLOAD_URL/$DOWNLOAD_VERSION" -o argo-$DOWNLOAD_VERSION.tar.gz
+mkdir -p $DOWNLOAD_DIRECTORY
+tar -xzf argo-$DOWNLOAD_VERSION.tar.gz --include "**/manifests" --directory $DOWNLOAD_DIRECTORY --strip-components 2
+rm -f argo-$DOWNLOAD_VERSION.tar.gz
