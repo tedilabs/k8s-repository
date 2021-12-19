fb0cfd14a528287058fc064d184efa623e0eaa8e
Byungjin Park <posquit0.bj@gmail.com>
2021-12-19 18:49:11 +0900

    Add kubernetes-external-secrets

---
+++ addons/kubernetes-external-secrets/download.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+
+DOWNLOAD_URL="https://api.github.com/repos/external-secrets/kubernetes-external-secrets/tarball"
+DOWNLOAD_VERSION="8.2.3"
+DOWNLOAD_DIRECTORY="$DOWNLOAD_VERSION"
+
+curl -Ls "$DOWNLOAD_URL/$DOWNLOAD_VERSION" -o $DOWNLOAD_VERSION.tar.gz
+mkdir -p $DOWNLOAD_DIRECTORY
+tar -xzf $DOWNLOAD_VERSION.tar.gz --include "**/charts/kubernetes-external-secrets" --directory $DOWNLOAD_DIRECTORY --strip-components 3
+rm -f $DOWNLOAD_VERSION.tar.gz
