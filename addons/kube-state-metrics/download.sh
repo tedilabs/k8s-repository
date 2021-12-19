6df1d74026721821af351461dab442d2b5ded153
Byungjin Park <posquit0.bj@gmail.com>
2021-12-19 18:48:58 +0900

    Add kube-state-metrics

---
+++ addons/kube-state-metrics/download.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+
+DOWNLOAD_URL="https://api.github.com/repos/kubernetes/kube-state-metrics/tarball"
+DOWNLOAD_VERSION="v2.1.0"
+DOWNLOAD_DIRECTORY="$DOWNLOAD_VERSION"
+
+curl -Ls "$DOWNLOAD_URL/$DOWNLOAD_VERSION" -o $DOWNLOAD_VERSION.tar.gz
+mkdir -p $DOWNLOAD_DIRECTORY
+tar -xzf $DOWNLOAD_VERSION.tar.gz --include "**/examples" --directory $DOWNLOAD_DIRECTORY --strip-components 2
+rm -f $DOWNLOAD_VERSION.tar.gz
