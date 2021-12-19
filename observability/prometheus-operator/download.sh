96774c738568eb94da084a70aa4e73787e7bec67
Byungjin Park <posquit0.bj@gmail.com>
2021-12-19 18:44:27 +0900

    Add prometheus-operator

---
+++ observability/prometheus-operator/download.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+
+DOWNLOAD_URL="https://api.github.com/repos/prometheus-operator/kube-prometheus/tarball"
+DOWNLOAD_VERSION="v0.7.0"
+DOWNLOAD_DIRECTORY="$DOWNLOAD_VERSION"
+
+curl -Ls "$DOWNLOAD_URL/$DOWNLOAD_VERSION" -o $DOWNLOAD_VERSION.tar.gz
+mkdir -p $DOWNLOAD_DIRECTORY
+tar -xzf $DOWNLOAD_VERSION.tar.gz --include "**/manifests/setup" --directory $DOWNLOAD_DIRECTORY --strip-components 3
+rm -f $DOWNLOAD_VERSION.tar.gz
