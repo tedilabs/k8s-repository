fd1c886993f714b160dcbc5a7ad90619b05ff2ad
Byungjin Park <posquit0.bj@gmail.com>
2022-02-26 23:37:11 +0900

    Add aws-load-balancer-controller v2.4 manifests

--- addons/aws-load-balancer-controller/download.sh
+++ addons/aws-load-balancer-controller/download.sh
@@ -3,9 +3,9 @@
 set -euf -o pipefail
 
 
-DOWNLOAD_URL="https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/main/docs/install"
-DOWNLOAD_VERSION="v2.2.3"
+DOWNLOAD_URL="https://github.com/kubernetes-sigs/aws-load-balancer-controller/releases/download"
+DOWNLOAD_VERSION="v2.4.0"
 DOWNLOAD_DIRECTORY="$DOWNLOAD_VERSION"
 
 mkdir -p $DOWNLOAD_DIRECTORY
-curl -Ls "$DOWNLOAD_URL/${DOWNLOAD_VERSION//./_}_full.yaml" -o $DOWNLOAD_DIRECTORY/install.yaml
+curl -Ls "$DOWNLOAD_URL/${DOWNLOAD_VERSION}/${DOWNLOAD_VERSION//./_}_full.yaml" -o $DOWNLOAD_DIRECTORY/install.yaml
