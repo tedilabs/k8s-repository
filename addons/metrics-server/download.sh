e49baf616c93ede52520fa476e5ee0502f308412
Byungjin Park <posquit0.bj@gmail.com>
2022-03-09 03:03:32 +0900

    Add metrics-server v0.6 manifests

--- addons/metrics-server/download.sh
+++ addons/metrics-server/download.sh
@@ -2,8 +2,8 @@
 
 set -euf -o pipefail
 
-DOWNLOAD_URL="https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.5.1/components.yaml"
-DOWNLOAD_VERSION="v0.5.1"
+DOWNLOAD_URL="https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.6.1/components.yaml"
+DOWNLOAD_VERSION="v0.6.1"
 DOWNLOAD_DIRECTORY="$DOWNLOAD_VERSION"
 
 mkdir -p $DOWNLOAD_DIRECTORY
