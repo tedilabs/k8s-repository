630e87cad10539100fff8f787e966891c150c006
Byungjin Park (Claud) <posquit0.bj@gmail.com>
2022-03-14 03:38:34 +0900

    Add rbac-manager v1.1 manifests (#8)

---
+++ addons/rbac-manager/download.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+DOWNLOAD_URL="https://api.github.com/repos/FairwindsOps/rbac-manager/tarball"
+DOWNLOAD_VERSION="v1.1.1"
+DOWNLOAD_DIRECTORY="$DOWNLOAD_VERSION"
+
+curl -Ls "$DOWNLOAD_URL/$DOWNLOAD_VERSION" -o $DOWNLOAD_VERSION.tar.gz
+mkdir -p $DOWNLOAD_DIRECTORY
+tar -xzf $DOWNLOAD_VERSION.tar.gz --include "**/deploy" --directory $DOWNLOAD_DIRECTORY --strip-components 2
+rm -f $DOWNLOAD_VERSION.tar.gz
