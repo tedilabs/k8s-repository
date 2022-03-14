db21869d3107a1b52840017c799ca34376af355a
Byungjin Park <posquit0.bj@gmail.com>
2022-03-14 16:59:27 +0900

    Add cert-manager v1.7 manifests

--- addons/cert-manager/download.sh
+++ addons/cert-manager/download.sh
@@ -4,8 +4,9 @@ set -euf -o pipefail
 
 
 DOWNLOAD_URL="https://github.com/jetstack/cert-manager/releases/download"
-DOWNLOAD_VERSION="v1.5.3"
+DOWNLOAD_VERSION="v1.7.1"
 DOWNLOAD_DIRECTORY="$DOWNLOAD_VERSION"
 
 mkdir -p $DOWNLOAD_DIRECTORY
 curl -Ls "$DOWNLOAD_URL/$DOWNLOAD_VERSION/cert-manager.yaml" -o $DOWNLOAD_DIRECTORY/cert-manager.yaml
+curl -Ls "$DOWNLOAD_URL/$DOWNLOAD_VERSION/cert-manager.crds.yaml" -o $DOWNLOAD_DIRECTORY/crd.yaml
