35f46eee198af18c6bba7efaa01f3b2c63c82b49
Byungjin Park <posquit0.bj@gmail.com>
2022-03-18 15:25:24 +0900

    Add atlantis manifests

---
+++ misc/atlantis/download.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+
+DOWNLOAD_URL="https://api.github.com/repos/runatlantis/atlantis/tarball"
+DOWNLOAD_VERSION="v0.18.1"
+DOWNLOAD_DIRECTORY="$DOWNLOAD_VERSION"
+
+curl -Ls "$DOWNLOAD_URL/$DOWNLOAD_VERSION" -o $DOWNLOAD_VERSION.tar.gz
+mkdir -p $DOWNLOAD_DIRECTORY
+tar -xzf $DOWNLOAD_VERSION.tar.gz --include "**/kustomize" --directory $DOWNLOAD_DIRECTORY --strip-components 2
+rm -f $DOWNLOAD_VERSION.tar.gz
