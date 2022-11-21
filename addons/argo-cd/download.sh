263a51fa38436c49d2e1ffd83f57cfbca82289c2
Byungjin Park <posquit0.bj@gmail.com>
2022-11-22 00:39:58 +0900

    Add notifications-catalog.yaml

--- addons/argo-cd/download.sh
+++ addons/argo-cd/download.sh
@@ -11,3 +11,5 @@ curl -Ls "$DOWNLOAD_URL/$DOWNLOAD_VERSION" -o argo-cd-$DOWNLOAD_VERSION.tar.gz
 mkdir -p $DOWNLOAD_DIRECTORY
 tar -xzf argo-cd-$DOWNLOAD_VERSION.tar.gz --include "**/manifests" --directory $DOWNLOAD_DIRECTORY --strip-components 2
 rm -f argo-cd-$DOWNLOAD_VERSION.tar.gz
+
+curl -Ls https://raw.githubusercontent.com/argoproj/argo-cd/${DOWNLOAD_VERSION}/notifications_catalog/install.yaml -o config-map.argocd-notifications-cm.yaml
