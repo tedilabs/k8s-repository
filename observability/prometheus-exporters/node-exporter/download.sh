2573660a93d0ff17b81f87173ba433be2953a6b8
Byungjin Park <posquit0.bj@gmail.com>
2022-12-07 03:01:27 +0900

    Add node-exporter v1.3.1 manifests

--- observability/prometheus-exporters/node-exporter/download.sh
+++ observability/prometheus-exporters/node-exporter/download.sh
@@ -4,10 +4,10 @@ set -euf -o pipefail
 
 
 DOWNLOAD_URL="https://api.github.com/repos/prometheus-operator/kube-prometheus/tarball"
-DOWNLOAD_VERSION="v0.7.0"
+DOWNLOAD_VERSION="v0.10.0"
 DOWNLOAD_DIRECTORY="$DOWNLOAD_VERSION"
 
 curl -Ls "$DOWNLOAD_URL/$DOWNLOAD_VERSION" -o $DOWNLOAD_VERSION.tar.gz
 mkdir -p $DOWNLOAD_DIRECTORY
-tar -xzf $DOWNLOAD_VERSION.tar.gz --include "**/manifests/node-exporter*" --directory $DOWNLOAD_DIRECTORY --strip-components 2
+tar -xzf $DOWNLOAD_VERSION.tar.gz --include "**/manifests/nodeExporter*" --directory $DOWNLOAD_DIRECTORY --strip-components 2
 rm -f $DOWNLOAD_VERSION.tar.gz
