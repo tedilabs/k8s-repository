1fa32b2c2bcd3aaf17545fa4c44bb41b6f2cf8ac
Byungjin Park <posquit0.bj@gmail.com>
2022-12-14 16:49:50 +0900

    Update metrics-server to v0.6.2

--- addons/metrics-server/download.sh
+++ addons/metrics-server/download.sh
@@ -3,7 +3,7 @@
 set -euf -o pipefail
 
 DOWNLOAD_URL="https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.6.1/components.yaml"
-DOWNLOAD_VERSION="v0.6.1"
+DOWNLOAD_VERSION="v0.6.2"
 DOWNLOAD_DIRECTORY="$DOWNLOAD_VERSION"
 
 mkdir -p $DOWNLOAD_DIRECTORY
