bad50ae081bdcfe02cdca104886972f034e54ec1
Byungjin Park <posquit0.bj@gmail.com>
2022-12-28 15:27:01 +0900

    Add prometheus v2.37 manifests

---
+++ observability/prometheus/download.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+DOWNLOAD_URL="https://api.github.com/repos/prometheus-operator/kube-prometheus/tarball"
+DOWNLOAD_VERSION="v0.11.0"
+DOWNLOAD_DIRECTORY="$DOWNLOAD_VERSION"
+
+curl -Ls "$DOWNLOAD_URL/$DOWNLOAD_VERSION" -o $DOWNLOAD_VERSION.tar.gz
+mkdir -p $DOWNLOAD_DIRECTORY
+tar -xzf $DOWNLOAD_VERSION.tar.gz --include "**/manifests/prometheus-*" --directory $DOWNLOAD_DIRECTORY --strip-components 2
+rm -f $DOWNLOAD_VERSION.tar.gz
