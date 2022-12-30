cdde5ad494af60ecc67b42259292b5e81d097ea6
Byungjin Park <posquit0.bj@gmail.com>
2022-12-30 17:59:17 +0900

    Add csi-snapshotter v6.2 manifests

--- addons/aws-ebs-csi-driver/download.sh
+++ addons/aws-ebs-csi-driver/download.sh
@@ -8,4 +8,4 @@ DOWNLOAD_DIRECTORY="$DOWNLOAD_VERSION"
 curl -Ls "https://github.com/kubernetes-sigs/aws-ebs-csi-driver/archive/refs/tags/$DOWNLOAD_VERSION.tar.gz" -o $DOWNLOAD_DIRECTORY.tar.gz
 mkdir -p $DOWNLOAD_DIRECTORY
 tar -xzf $DOWNLOAD_VERSION.tar.gz --include "**/deploy/kubernetes" --directory $DOWNLOAD_DIRECTORY --strip-components 3
-rm -f argo-$DOWNLOAD_VERSION.tar.gz
+rm -f $DOWNLOAD_VERSION.tar.gz
---
+++ addons/csi-snapshotter/download.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+DOWNLOAD_VERSION="v6.2.0"
+DOWNLOAD_DIRECTORY="$DOWNLOAD_VERSION"
+
+curl -Ls "https://github.com/kubernetes-csi/external-snapshotter/archive/refs/tags/$DOWNLOAD_VERSION.tar.gz" -o $DOWNLOAD_DIRECTORY.tar.gz
+mkdir -p $DOWNLOAD_DIRECTORY
+tar -xzf $DOWNLOAD_VERSION.tar.gz --include "**/deploy/kubernetes/snapshot-controller" --include "**/client/config/crd" --directory $DOWNLOAD_DIRECTORY --strip-components 3
+rm -f $DOWNLOAD_VERSION.tar.gz
