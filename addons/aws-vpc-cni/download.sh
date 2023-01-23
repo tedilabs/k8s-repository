f1b7415d7d129d4bd0ac1df60178ad0edcdf730a
Byungjin Park <posquit0.bj@gmail.com>
2023-01-23 22:49:22 +0900

    Update aws-vpc-cni to v1.12.1

--- addons/aws-vpc-cni/download.sh
+++ addons/aws-vpc-cni/download.sh
@@ -4,7 +4,7 @@ set -euf -o pipefail
 
 
 DOWNLOAD_URL="https://api.github.com/repos/aws/amazon-vpc-cni-k8s/tarball"
-DOWNLOAD_VERSION="v1.12.0"
+DOWNLOAD_VERSION="v1.12.1"
 DOWNLOAD_DIRECTORY="$DOWNLOAD_VERSION"
 
 curl -Ls "$DOWNLOAD_URL/$DOWNLOAD_VERSION" -o $DOWNLOAD_VERSION.tar.gz
